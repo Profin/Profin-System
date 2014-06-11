20591b3ea905ec1e237525eb70b55be6a692c780
keenanbaartman <keenanbaartman@gmail.com>
2014-06-11 14:11:30 +0200

    DataAccessLayerClass Additions
    Added Insert statements and Classes to the DAL

---
+++ ProfinSystem/DataAccessLayer/CompanyClass.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    class CompanyClass
+    {
+        public CompanyClass() { }
+
+        string companyID;
+
+        public string CompanyID
+        {
+            get { return companyID; }
+            set { companyID = value; }
+        }
+
+        string companyName;
+
+        public string CompanyName
+        {
+            get { return companyName; }
+            set { companyName = value; }
+        }
+
+        string registrationNum;
+
+        public string RegistrationNum
+        {
+            get { return registrationNum; }
+            set { registrationNum = value; }
+        }
+
+        string fspNum;
+
+        public string FspNum
+        {
+            get { return fspNum; }
+            set { fspNum = value; }
+        }
+
+        string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        string surname;
+
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        string initials;
+
+        public string Initials
+        {
+            get { return initials; }
+            set { initials = value; }
+        }
+
+        string designation;
+
+        public string Designation
+        {
+            get { return designation; }
+            set { designation = value; }
+        }
+
+        string phoneNo;
+
+        public string PhoneNo
+        {
+            get { return phoneNo; }
+            set { phoneNo = value; }
+        }
+
+        string faxNo;
+
+        public string FaxNo
+        {
+            get { return faxNo; }
+            set { faxNo = value; }
+        }
+
+        string cellNo;
+
+        public string CellNo
+        {
+            get { return cellNo; }
+            set { cellNo = value; }
+        }
+
+        string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        string address;
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        string region;
+
+        public string Region
+        {
+            get { return region; }
+            set { region = value; }
+        }
+
+        string suburb;
+
+        public string Suburb
+        {
+            get { return suburb; }
+            set { suburb = value; }
+        }
+
+        string town;
+
+        public string Town
+        {
+            get { return town; }
+            set { town = value; }
+        }
+
+        string postalCode;
+
+        public string PostalCode
+        {
+            get { return postalCode; }
+            set { postalCode = value; }
+        }
+
+    }
+}
---
+++ ProfinSystem/DataAccessLayer/EmployeeClass.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    class EmployeeClass
+    {
+        public EmployeeClass() { }
+
+        string empID;
+
+        public string EmpID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+
+        string departmentID;
+
+        public string DepartmentID
+        {
+            get { return departmentID; }
+            set { departmentID = value; }
+        }
+
+        string raceID;
+
+        public string RaceID
+        {
+            get { return raceID; }
+            set { raceID = value; }
+        }
+
+        string qualificationID;
+
+        public string QualificationID
+        {
+            get { return qualificationID; }
+            set { qualificationID = value; }
+        }
+
+        string companyID;
+
+        public string CompanyID
+        {
+            get { return companyID; }
+            set { companyID = value; }
+        }
+
+        string occDescID;
+
+        public string OccDescID
+        {
+            get { return occDescID; }
+            set { occDescID = value; }
+        }
+
+        string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        string address;
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        DateTime dateOfBirth;
+
+        public DateTime DateOfBirth
+        {
+            get { return dateOfBirth; }
+            set { dateOfBirth = value; }
+        }
+
+        string gender;
+
+        public string Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+
+        string cell;
+
+        public string Cell
+        {
+            get { return cell; }
+            set { cell = value; }
+        }
+
+        string homeTel;
+
+        public string HomeTel
+        {
+            get { return homeTel; }
+            set { homeTel = value; }
+        }
+
+        string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        string jobTitle;
+
+        public string JobTitle
+        {
+            get { return jobTitle; }
+            set { jobTitle = value; }
+        }
+
+        string appointDate;
+
+        public string AppointDate
+        {
+            get { return appointDate; }
+            set { appointDate = value; }
+        }
+
+        string terminationDate;
+
+        public string TerminationDate
+        {
+            get { return terminationDate; }
+            set { terminationDate = value; }
+        }
+
+        float hourlyRate;
+
+        public float HourlyRate
+        {
+            get { return hourlyRate; }
+            set { hourlyRate = value; }
+        }
+
+        string userName;
+
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+
+        string userPassword;
+
+        public string UserPassword
+        {
+            get { return userPassword; }
+            set { userPassword = value; }
+        }
+
+        string fais;
+
+        public string Fais
+        {
+            get { return fais; }
+            set { fais = value; }
+        }
+
+        string ageGroup;
+
+        public string AgeGroup
+        {
+            get { return ageGroup; }
+            set { ageGroup = value; }
+        }
+
+        string citizenship;
+
+        public string Citizenship
+        {
+            get { return citizenship; }
+            set { citizenship = value; }
+        }
+
+        string region;
+
+        public string Region
+        {
+            get { return region; }
+            set { region = value; }
+        }
+
+        string suburb;
+
+        public string Suburb
+        {
+            get { return suburb; }
+            set { suburb = value; }
+        }
+
+        string town;
+
+        public string Town
+        {
+            get { return town; }
+            set { town = value; }
+        }
+
+        string postalCode;
+
+        public string PostalCode
+        {
+            get { return postalCode; }
+            set { postalCode = value; }
+        }
+
+    }
+}
